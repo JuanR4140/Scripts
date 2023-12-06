0ef034ee340d72a92ea6ab2125ba5f20059c7911
JuanR4140 <N/A>
2023-12-06 13:04:50 -0600

    Finished user management script. Will add mroe maybe

--- Linux/user_management.sh
+++ Linux/user_management.sh
@@ -74,7 +74,107 @@ then
         exit
     fi
 
-    echo " ... "
+    echo -e "Checking $users_file..\n"
+    not_found_amnt=0
+    root_rights_amnt=0
+    while read -r line
+    do
+        not_found=true
+        # echo "$line"
+        while IFS=' ' read -ra ADDR; do
+            for user in "${ADDR[@]}"; do
+                # echo -n "$user"
+                if [ $line == $user  ]; then
+                    not_found=false
+
+                    if [[ $(groups $line) == *"$sudo_group"* ]] then
+                        ((root_rights_amnt++))
+                        echo -e "$line found, but has ADMIN PERMISSIONS\n"
+                    fi
+                fi
+            done
+        done <<< "$(ls /home)"
+
+        if $not_found; then
+            ((not_found_amnt++))
+            echo -e "$line NOT found in provided user list.\n"
+        fi
+
+    done < "$users_file"
+
+    echo -e "$users_file RESULTS\n----------------------\n"
+    echo -e "$not_found_amnt users not found in provided user list."
+    echo -e "Double check the results are correct, then delete the user with deluser.\n"
+    echo -e "$root_rights_amnt users found in provided user list, but are ADMINS."
+    echo -e "Double check the results are correct, then remove the user from $sudo_group with gpasswd.\n"
+
+    echo -e "[Press ENTER to continue]"
+    read trash
+
+
+    echo -e "Checking $admin_file..\n"
+    not_found_amnt=0
+    root_rights_amnt=0
+    while read -r line
+    do
+        # echo $line
+        not_found=true
+        while IFS=' ' read -ra ADDR; do
+            for user in "${ADDR[@]}"; do
+                # echo $user
+                if [ $line == $user ]; then
+                    not_found=false
+
+                    if [[ $(groups $line) != *"$sudo_group"* ]]; then
+                        echo -e "$line found, but has STANDARD PERMISSIONS\n"
+                    fi
+                fi
+            done
+        done <<< $(ls /home) 
+
+        if $not_found; then
+            ((not_found_amnt++))
+            echo -e "$line NOT found in provided admin list.\n"
+        fi
+
+    done < "$admin_file"
+
+    echo -e "$admin_file RESULTS\n----------------------\n"
+    echo -e "$not_found_amnt users not found in provided admin list."
+    echo -e "Double check the results are correct, then delete the user with deluser.\n"
+    echo -e "$root_rights_amnt users found in provided admin list, but have STANDARD permission."
+    echo -e "Double check the results are correct, then add the user to $sudo_group with gpasswd.\n\n\n"
+
+
+    not_found_amnt=0
+    while IFS=' ' read -ra ADDR; do
+        for user in "${ADDR[@]}"; do
+            not_found=true
+            while read -r line
+            do
+                if [ $user == $line  ]; then
+                    not_found=false
+                fi
+            done < $users_file
+
+            while read -r line
+            do
+                if [ $user == $line  ]; then
+                    not_found=false
+                fi
+            done < $admin_file
+
+            if $not_found; then
+                ((not_found_amnt++))
+                echo -e "$user NOT PRESENT WITHIN ANY GIVEN FILE.\n"
+            fi
+
+        done
+    done <<< $(ls /home)
+
+    echo -e "\n$not_found_amnt USERS NOT FOUND WITHIN ANY GIVEN FILE.\n"
+
+    exit
 
 else
     echo -e "Invalid option.\n"
